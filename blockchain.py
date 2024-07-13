ceace66aeed83c2aba90c742c1b376879febefca
ben416123 <c3045540@newcastle.ac.uk>
2024-07-13 15:18:30 +0100

    Basic blockchain implemented

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,7 @@
 import hashlib
 import json
 from time import time
-
-from time import time
 from uuid import uuid4
-
 from flask import Flask, jsonify, request
 
 
@@ -13,7 +10,7 @@ class Blockchain(object):
         self.chain = []
         self.current_transactions = []
 
-        self.new_block(previous_hash=1, proof=100)
+        self.new_block(previous_hash='1', proof=100)
 
     def new_block(self, proof, previous_hash=None):
         """
@@ -110,19 +107,28 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return "We'll mine a new Block"
+    # We run the proof of work algorithm to get the next proof...
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
 
+    # We must receive a reward for finding the proof.
+    # The sender is "0" to signify that this node has mined a new coin.
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
 
-@app.route('/transactions/new', methods=['POST'])
-def new_transaction():
-    return "We'll add a new transaction"
+    # Forge the new Block by adding it to the chain
+    block = blockchain.new_block(proof)
 
-
-@app.route('/chain', methods=['GET'])
-def full_chain():
     response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain),
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
     }
     return jsonify(response), 200
 
@@ -141,6 +147,15 @@ def new_transaction():
 
     response = {'message': f'Transaction will be added to Block {index}'}
     return jsonify(response), 201
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
 
 
 if __name__ == '__main__':
